7f00f3ed919d01163154e2f2f8073eec066736f1
phinst <33879604+phinst@users.noreply.github.com>
2017-12-14 18:37:57 +0100

    add display functions

---
+++ display_mgmnt.c
@@ -0,0 +1,7 @@
+#include "display_mgmnt.h"
+#include <p33FJ128GP802.h>
+//Prozessorspezifische Headerdatei
+
+/*I2C Backpack: PCF8574AT, K44001, 05, KNM00463
+ *
+ */
---
+++ display_mgmnt.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   display_mgmnt.h
+ * Author: finst
+ *
+ * Created on 13. Dezember 2017, 20:00
+ */
+
+#ifndef DISPLAY_MGMNT_H
+#define	DISPLAY_MGMNT_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* DISPLAY_MGMNT_H */
+
